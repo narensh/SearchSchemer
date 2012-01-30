f5d96a8c797a168a00aa13fa8322fb5646bbe585
Rafał Kuć <rafal@kuciowie.pl>
2012-01-30 17:06:42 +0100

    SolrIndexStructureWriter implementation.

--- src/main/java/com/sematext/searchschemer/writer/SolrIndexStructureWriter.java
+++ src/main/java/com/sematext/searchschemer/writer/SolrIndexStructureWriter.java
@@ -1,24 +1,81 @@
 package com.sematext.searchschemer.writer;
 
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.Writer;
+import java.util.Map;
 
+import com.sematext.searchschemer.index.FieldAttributes;
 import com.sematext.searchschemer.index.IndexStructure;
 
-/** 
+/**
  * Implementation of {@link IndexStructureWriter} for Apache Solr.
  * 
  * @author Sematext
- *
+ * 
  */
 public class SolrIndexStructureWriter implements IndexStructureWriter {
   protected SolrIndexStructureWriter() {
   }
-  
-  /** 
+
+  /**
    * {@inheritDoc}
    */
   @Override
   public void write(IndexStructure structure, String fileName) throws IOException {
-    //TODO implementation
+    FileWriter writer = new FileWriter(fileName);
+    write(structure, writer);
+    writer.flush();
+    writer.close();
+  }
+
+  /**
+   * Writes index structure.
+   * 
+   * @param structure
+   *          index structure
+   * @param writer
+   *          writer instance
+   * @throws IOException
+   *           thrown when I/O error occurs
+   */
+  protected void write(IndexStructure structure, Writer writer) throws IOException {
+    if (!structure.fields().isEmpty()) {
+      for (Map.Entry<String, FieldAttributes> field : structure.fields().entrySet()) {
+        writeField("<field", field.getKey(), field.getValue(), writer);
+      }
+    }
+    if (!structure.dynamicFields().isEmpty()) {
+      for (Map.Entry<String, FieldAttributes> field : structure.dynamicFields().entrySet()) {
+        writeField("<dynamicField", field.getKey(), field.getValue(), writer);
+      }
+    }
+  }
+
+  /**
+   * Writes a single schema field.
+   * 
+   * @param prefix
+   *          prefix for a given field
+   * @param fieldName
+   *          name of the field
+   * @param attr
+   *          field attributes
+   * @param writer
+   *          writer to write to
+   * @throws IOException
+   *           thrown when I/O error occurs
+   */
+  protected void writeField(String prefix, String fieldName, FieldAttributes attr, Writer writer) throws IOException {
+    writer.write(prefix);
+    writer.write(" name=\"");
+    writer.write(fieldName);
+    writer.write("\" type=\"");
+    writer.write(attr.getType());
+    writer.write("\" indexed=\"");
+    writer.write(attr.getIndexed().toString().toLowerCase());
+    writer.write("\" stored=\"");
+    writer.write(attr.getStored().toString().toLowerCase());
+    writer.write("\" />\n");
   }
 }
---
+++ src/test/java/com/sematext/searchschemer/writer/SolrIndexStructureWriterTest.java
@@ -0,0 +1,56 @@
+package com.sematext.searchschemer.writer;
+
+import java.io.StringWriter;
+
+import junit.framework.TestCase;
+
+import org.junit.Test;
+
+import com.sematext.searchschemer.index.BasicIndexStructure;
+import com.sematext.searchschemer.index.FieldAttributes;
+import com.sematext.searchschemer.index.IndexStructure;
+
+public class SolrIndexStructureWriterTest extends TestCase {
+  @Test
+  public void testWriteNonDynamic() throws Exception {
+    IndexStructure structure = new BasicIndexStructure();
+    structure.addField("cat", new FieldAttributes("string", true, true), false);
+
+    StringWriter writer = new StringWriter();
+    SolrIndexStructureWriter solrIndexStructurWriter = new SolrIndexStructureWriter();
+    solrIndexStructurWriter.write(structure, writer);
+
+    assertEquals("<field name=\"cat\" type=\"string\" indexed=\"true\" stored=\"true\" />\n", writer.toString());
+    writer.close();
+  }
+
+  @Test
+  public void testWriteDynamic() throws Exception {
+    IndexStructure structure = new BasicIndexStructure();
+    structure.addField("*_int", new FieldAttributes("string", false, true), true);
+
+    StringWriter writer = new StringWriter();
+    SolrIndexStructureWriter solrIndexStructurWriter = new SolrIndexStructureWriter();
+    solrIndexStructurWriter.write(structure, writer);
+
+    assertEquals("<dynamicField name=\"*_int\" type=\"string\" indexed=\"false\" stored=\"true\" />\n",
+        writer.toString());
+    writer.close();
+  }
+
+  @Test
+  public void testWriteMultiple() throws Exception {
+    IndexStructure structure = new BasicIndexStructure();
+    structure.addField("cat", new FieldAttributes("string", true, true), false);
+    structure.addField("menu", new FieldAttributes("string", false, true), false);
+
+    StringWriter writer = new StringWriter();
+    SolrIndexStructureWriter solrIndexStructurWriter = new SolrIndexStructureWriter();
+    solrIndexStructurWriter.write(structure, writer);
+
+    assertEquals(
+        "<field name=\"cat\" type=\"string\" indexed=\"true\" stored=\"true\" />\n<field name=\"menu\" type=\"string\" indexed=\"false\" stored=\"true\" />\n",
+        writer.toString());
+    writer.close();
+  }
+}
